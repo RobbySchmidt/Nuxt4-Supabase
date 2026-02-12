02e5c755adf973fbb38e376b6876ee69fe6362b8
RobbySchmidt <schmidt@rhowerk.de>
2026-02-12 10:59:23 +0100

    updating data naming

--- app/pages/[slug].vue
+++ app/pages/[slug].vue
@@ -1,8 +1,9 @@
 <template>
   <div class="pb-f-24 space-y-f-24">
+    <pre>{{ page }}</pre>
     <component
-      v-if="page.content"
-      v-for="block in page.content"
+      v-if="page.components"
+      v-for="block in page.components"
       :key="block.sort"
       :is="components[block.type?.type]"
       :content="block.content"
